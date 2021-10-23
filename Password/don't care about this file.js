fc40bef0182cafc484864c742c467e0d1e543c6e
PhucTan-IT <87054949+PhucTan-IT@users.noreply.github.com>
2021-10-23 14:03:49 +0700

    Delete Password directory

--- Password/don't care about this file.js
+++
@@ -1,6 +0,0 @@
-// CONSOLE
-console.log("Welcome to console of password file");
-
-document.getElementById("ok").onclick = function() {
-	console.log("So, you have understand and you had the password. Have a good game!")
-}	
